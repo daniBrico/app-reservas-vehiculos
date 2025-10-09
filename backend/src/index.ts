65d6fe534061a396e916c74037145814cd03651a
Daniel <daniel.jorge96@outlook.com>
2025-10-09 16:58:50 -0300

    Actualización configuración inicial del backend

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import { createServer } from 'node:http'
+import { app } from './app.js'
+
+const port = process.env.PORT ?? 3000
+
+const server = createServer(app)
+
+server.listen(port, () => {
+  console.log(`server running on port http://localhost:${port}`)
+})
