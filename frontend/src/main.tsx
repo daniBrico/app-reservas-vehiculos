d89158ae7efe052700a17affbe7943c36794f7c2
Daniel <daniel.jorge96@outlook.com>
2025-10-29 02:00:33 -0300

    Cambio de fuente

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import '@fontsource-variable/noto-sans-bengali'
+import '@fontsource/questrial'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
